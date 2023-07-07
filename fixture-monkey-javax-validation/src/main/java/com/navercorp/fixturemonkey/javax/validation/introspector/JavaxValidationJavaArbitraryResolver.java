1fdd403fe21e9908c9663f81af8d929cac64e506
sangy515 <sangy515@mit.edu>
2023-07-07 15:19:19 +0900

    add MonkeyStringArbitrary features
    Fix the location of filterCharacter
    
    Add comments, fix style
    
    Fix the location of filterCharacter

--- fixture-monkey-javax-validation/src/main/java/com/navercorp/fixturemonkey/javax/validation/introspector/JavaxValidationJavaArbitraryResolver.java
+++ fixture-monkey-javax-validation/src/main/java/com/navercorp/fixturemonkey/javax/validation/introspector/JavaxValidationJavaArbitraryResolver.java
@@ -45,6 +45,7 @@ import net.jqwik.api.arbitraries.ShortArbitrary;
 import net.jqwik.api.arbitraries.StringArbitrary;
 import net.jqwik.web.api.Web;
 
+import com.navercorp.fixturemonkey.api.arbitrary.MonkeyStringArbitrary;
 import com.navercorp.fixturemonkey.api.generator.ArbitraryGeneratorContext;
 import com.navercorp.fixturemonkey.api.introspector.JavaArbitraryResolver;
 
@@ -87,7 +88,7 @@ public final class JavaxValidationJavaArbitraryResolver implements JavaArbitrary
 			return Arbitraries.of(values);
 		}
 
-		Arbitrary<String> arbitrary;
+		Arbitrary<String> arbitrary = stringArbitrary;
 		if (context.findAnnotation(Email.class).isPresent()) {
 			arbitrary = Web.emails().allowIpv4Host();
 			if (min != null) {
@@ -118,8 +119,6 @@ public final class JavaxValidationJavaArbitraryResolver implements JavaArbitrary
 				if (!notBlank) {
 					if (it == null) {
 						return true;
-					} else {
-						return !containsControlCharacters(it);
 					}
 				}
 
@@ -127,7 +126,7 @@ public final class JavaxValidationJavaArbitraryResolver implements JavaArbitrary
 					return false;
 				}
 
-				return !containsControlCharacters(it);
+				return true;
 			});
 	}
 
